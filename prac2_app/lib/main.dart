65f699c82cc21af043439881256be9f3110feeb1
KHJcode <kimstudswcode4485@gmail.com>
2020-05-04 01:30:34 +0900

    200504

--- prac2_app/lib/main.dart
+++ prac2_app/lib/main.dart
@@ -6,31 +6,132 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'First app',
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
-    home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      title: 'PRACTICE',
+      home: Grade(),
     );
   }
 }
-
-class MyHomePage extends StatelessWidget {
+class Grade extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amber[800],
       appBar: AppBar(
-        title: Text('Prac2 app'),
+        title: Text('PRACTICE'),
+        backgroundColor: Colors.amber[700],
+        centerTitle: true,
+        elevation: 0.0,
       ),
-      body: Center(
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text('Hello'),
-            Text('Hello'),
-            Text('Hello'),
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/github.gif'),
+                radius: 60.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[850],
+              thickness: 0.5,
+              endIndent: 30.0,
+            ),
+            Text('NAME',
+            style: TextStyle(
+              color: Colors.white,
+              letterSpacing: 2.0,
+            ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text('PRACTICE',
+            style: TextStyle(
+              color: Colors.white,
+              letterSpacing: 2.0,
+              fontSize: 28.0,
+              fontWeight: FontWeight.bold
+            ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text('POWER LEVEL',
+            style: TextStyle(
+              color: Colors.white,
+              letterSpacing: 2.0,
+            ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text('14',
+            style: TextStyle(
+              color: Colors.white,
+              letterSpacing: 2.0,
+              fontSize: 28.0,
+              fontWeight: FontWeight.bold
+            ),
+            ),
+            SizedBox(
+              height: 30.0 
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0 
+                ),
+                Text('skill 1',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  letterSpacing: 1.0
+                )
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0 
+                ),
+                Text('skill 2',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  letterSpacing: 1.0
+                )
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0 
+                ),
+                Text('skill 3',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  letterSpacing: 1.0
+                )
+                ),
+              ],
+            ),
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/logo.png'),
+                radius: 40.0,
+                backgroundColor: Colors.amber[800],
+              ),
+            ),
           ],
-        ),
-      ),
+        )
+      ,)
     );
   }
 }
