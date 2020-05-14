f2fcae9794f42c56e1d9d458067849e81ef151c8
KHJcode <kimstudswcode4485@gmail.com>
2020-05-15 01:57:34 +0900

    200515

---
+++ login_view/lib/signin_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(child: Container(decoration: BoxDecoration(image: ),))
+        ],
+      )
+    );
+  }
+}
+// 7분까지
